36c9a26b9b6db847c4d421cdffed5c1cd458ba65
OmidrezaBagherian <OmidCoder21@gmail.com>
2022-05-15 09:27:45 -0700

    Add Navigation Component And fix Xmls in project

--- app/src/main/java/ir/omidrezabagherian/filmapplication/ui/MainActivity.kt
+++ app/src/main/java/ir/omidrezabagherian/filmapplication/ui/MainActivity.kt
@@ -2,14 +2,40 @@ package ir.omidrezabagherian.filmapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import ir.omidrezabagherian.filmapplication.R
+import ir.omidrezabagherian.filmapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+    private val navController by lazy {
+        findNavController(R.id.fragmentContainerViewMain)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
 
+        mainBinding.bottomNavigationViewMain.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.tabHome -> {
+                    navController.navigate(R.id.homeFragment)
+                }
 
-        setContentView(R.layout.activity_main)
+                R.id.tabComingSoon -> {
+                    navController.navigate(R.id.comingSoonFragment)
+                }
+
+                R.id.tabPlay -> {
+                    navController.navigate(R.id.playerFragment)
+                }
+            }
+
+            true
+        }
+
+        setContentView(mainBinding.root)
     }
 }
